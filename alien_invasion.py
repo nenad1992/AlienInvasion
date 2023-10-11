277f56f19056f4e49210ed7eb6b5f3b08a49e6ce
Nenad <nenadj92@gmail.com>
2023-10-11 12:50:06 +0200

    Refactoring _create_fleet()

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,6 +80,13 @@ class AlienInvasion:
               if bullet.rect.bottom <= 0:
                    self.bullets.remove(bullet)
 
+    def _create_alien(self, x_position):
+         """Create an alien and place it in the row."""
+         new_alien = Alien(self)
+         new_alien.x = x_position
+         new_alien.rect.x = x_position
+         self.aliens.add(new_alien)
+
     def _create_fleet(self):
          """Create the fleet of aliens."""
          # Create an alien and keep adding aliens until there's no room left.
@@ -89,10 +96,7 @@ class AlienInvasion:
 
          current_x = alien_width
          while current_x < (self.settings.screen_width - 2 * alien_width):
-              new_alien = Alien(self)
-              new_alien.x = current_x
-              new_alien.rect.x = current_x
-              self.aliens.add(new_alien)
+              self._create_alien(current_x)
               current_x += 2 * alien_width
 
     def _update_screen(self):
